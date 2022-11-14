c57f6ffeb3156e27a35c03fdfafbec260fb970f9
Filip <filip.cosic91@gmail.com>
2022-11-14 18:27:11 +0100

    Add project files.

---
+++ Phonebook/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Phonebook.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
